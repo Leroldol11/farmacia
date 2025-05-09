08026f8a87df1aaeb47107052977ff6af5d20ecb
Jhon Jesus Jaimes Rojas <jhonjaimes1100@gmail.com>
2025-05-08 22:31:48 -0400

    un cambio en configuracion para el login

--- farmacia/settings.py
+++ farmacia/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework', 
     'api.apps.ApiConfig',  # 'api',
+    'rest_framework_simplejwt',
     #'rest_framework.authtoken',
 
 ]
